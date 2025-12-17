fef2892099b42b76099e22a10b545b368b1eb52c
tsarevartem19-bit <tsarevartem188@gmail.com>
2025-12-18 00:53:51 +0300

    Задание 2: Создал массив из 5 товаров

--- Homework/Lesson 2.3/Lesson 2 Homework/org/example/MainProduct.java
+++ Homework/Lesson 2.3/Lesson 2 Homework/org/example/MainProduct.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 public class MainProduct {
     public static void main(String[] args) {
         Product[] productsArray = new Product[5];
-        productsArray[0] = new Product("Samsung S25 Ultra", LocalDate.of(2025, 2, 1), "Samsung Corp.", "Korea", 5599, true);
+        productsArray[0] = new Product("Samsung S25 Ultrra", LocalDate.of(2025, 2, 1), "Samsung Corp.", "Korea", 5599, true);
         productsArray[1] = new Product("iPhone 14", LocalDate.of(2025, 3, 10), "Apple Inc.", "USA", 6999, false);
         productsArray[2] = new Product("Xiaomi 12", LocalDate.of(2025, 4, 15), "Xiaomi Corp.", "China", 2999, true);
         productsArray[3] = new Product("Huawei P50", LocalDate.of(2025, 5, 20), "Huawei", "China", 3999, false);
